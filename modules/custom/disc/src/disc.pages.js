942af882349b7c6f2e3644a389dd41a2280dc12d
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-17 22:14:44 -0500

    Add a map to the courses page.

--- modules/custom/disc/src/disc.pages.js
+++ modules/custom/disc/src/disc.pages.js
@@ -1,10 +1,19 @@
+// Create global variables to hold coordinates and the map.
+var _disc_user_latitude = null;
+var _disc_user_longitude = null;
+var _disc_map = null;
+
 function disc_courses_page_controller() {
   return new Promise(function(ok, err) {
 
     var content = {};
+
+    // Show a button to add a course.
     content['add_course_button'] = {
       _markup: dg.l('Add a course', 'add-course', { _attributes: { 'class': ['button'] } })
     };
+
+    // Show a list of courses.
     content['course_list'] = {
       _theme: 'view',
       _path: 'courses', // Path to the View in Drupal
@@ -18,46 +27,25 @@ function disc_courses_page_controller() {
       }
     };
 
+    // Add a placeholder for the map.
+    var map_attributes = {
+      id: 'my-module-map',
+      style: 'width: 100%; height: 320px;'
+    };
+    content['map'] = {
+      _markup: '<div ' + dg.attributes(map_attributes) + '></div>',
+      _postRender: [disc_map_post_render]
+    };
+
     navigator.geolocation.getCurrentPosition(
       // Success
       function (position) {
 
-        // Inspect the position coordinates.
-        //console.log(position.coords);
+        // Set aside the user's position.
+        _disc_user_latitude = position.coords.latitude;
+        _disc_user_longitude = position.coords.longitude;
 
-        // Show a generic alert message.
-        //dg.alert(
-        //    'Latitude: '          + position.coords.latitude          + '\n' +
-        //    'Longitude: '         + position.coords.longitude         + '\n' +
-        //    'Altitude: '          + position.coords.altitude          + '\n' +
-        //    'Accuracy: '          + position.coords.accuracy          + '\n' +
-        //    'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-        //    'Heading: '           + position.coords.heading           + '\n' +
-        //    'Speed: '             + position.coords.speed             + '\n' +
-        //    'Timestamp: '         + position.timestamp                + '\n'
-        //);
 
-        // Show a bullet list of coordinate data...
-
-        // Iterate over each coordinate and place them in a list.
-        var pieces = ['latitude', 'longitude', 'altitude', 'accuracy', 'altitudeAccuracy', 'heading', 'speed'];
-        var items = [];
-        for (var i = 0; i < pieces.length; i++) {
-          var piece = pieces[i];
-          items.push(piece + ': ' + position.coords[piece])
-        }
-
-        // Show the date and time the location was retrieved.
-        var d = new Date(position.timestamp);
-        content['user-location-date'] = {
-          _markup: d.toDateString()
-        };
-
-        // Show the pieces of the coordinates.
-        content['user-location'] = {
-          _theme: 'item_list',
-          _items: items
-        };
 
         ok(content);
 
@@ -68,8 +56,49 @@ function disc_courses_page_controller() {
             'Code: '    + error.code    + '\n' +
             'Message: ' + error.message + '\n'
         );
+        ok(content);
       }
     );
 
+  });
+}
+
+function disc_map_post_render() {
+
+  // Build the lat lng object from the user's position.
+  var myLatlng = new google.maps.LatLng(
+      _disc_user_latitude,
+      _disc_user_longitude
+  );
+
+  // Set the map's options.
+  var mapOptions = {
+    center: myLatlng,
+    zoom: 11,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.SMALL
+    }
+  };
+
+  // Initialize the map, and set a timeout to resize properly.
+  _disc_map = new google.maps.Map(
+      document.getElementById("my-module-map"),
+      mapOptions
+  );
+  setTimeout(function() {
+    google.maps.event.trigger(_disc_map, 'resize');
+    _disc_map.setCenter(myLatlng);
+  }, 500);
+
+  // Add a marker for the user's current position.
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: _disc_map,
+    icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
   });
 }
